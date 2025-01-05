c97719fd73bfaa143dbb8aed6de9dd739900eae8
Mohammed taqi <153135847+mohmeadtke@users.noreply.github.com>
2025-01-05 19:16:30 +0300

    5 day

--- Features/auth/Presentation/Widget/bloc_form.dart
+++ Features/auth/Presentation/Widget/bloc_form.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:teamapp/Features/auth/Presentation/pages/verifie_page.dart';
 import '../state_mangment/bloc/auth_bloc.dart';
 import '../state_mangment/bloc/auth_state.dart';
 import 'form.dart';
